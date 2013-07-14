9caa6006c3b87923f3441de401fc08edf3b340c6
George Li <gzli92@gmail.com>
2013-07-14 17:30:45 -0500

    tried to add directional shooting

--- Player_Shoot.cs
+++ Player_Shoot.cs
@@ -7,21 +7,35 @@ public class Player_Shoot : MonoBehaviour {
 	public float speed = 20;
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//keyboard input
-		if (Input.GetButtonDown("Fire1"))
+		if (Input.GetButtonDown("Fire1") || Input.GetKeyDown("space"))
 		{
 			
+			Player_Movement movement = new Player_Movement();
+			//Player_Movement movement = GetComponent(Player_Movement); NOT WORKING.
 			
-			Rigidbody instantiatedProjectile = Instantiate(projectile,transform.position,transform.rotation *  Quaternion.Euler(90, 0, 0)) as Rigidbody;
+			Rigidbody instantiatedProjectile = Instantiate(projectile,transform.position,transform.rotation *  Quaternion.Euler(270, 0, 0)) as Rigidbody;
 			
 			//create movement
-			instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
 			
+			//Vector3 shootVector = new Vector3(0,0,speed);
+			//instantiatedProjectile.velocity = transform.TransformDirection(shootVector);
+			if(movement.Horiz != 0){
+				if(movement.Horiz > 0){
+					instantiatedProjectile.AddForce(transform.right * speed*2, ForceMode.Force);
+				}
+				else{
+					instantiatedProjectile.AddForce(-transform.right * speed*2, ForceMode.Force);
+				}
+			}
+			else{
+				instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0,instantiatedProjectile.velocity.y*2,speed)); 
+			}
 			//disable collisions between object and player
 			//Physics.IgnoreCollision(instantiatedProjectile.collider, transform.root.collider);
 			//instantiatedProjectile.collider.on
