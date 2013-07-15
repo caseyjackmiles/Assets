e03f132a639fa5ed03c3a64dd5827b0574c7294a
George Li <gzli92@gmail.com>
2013-07-14 19:55:54 -0500

    attempted directional shooting

--- Player_Shoot.cs
+++ Player_Shoot.cs
@@ -5,9 +5,12 @@ public class Player_Shoot : MonoBehaviour {
 	public Rigidbody projectile;
 	
 	public float speed = 20;
+	private PlayerAnimationController animation;
+	//enum for character state
+   
 	// Use this for initialization
 	void Start () {
-		
+		animation = GetComponent<PlayerAnimationController>();
 	}
 	
 	// Update is called once per frame
@@ -16,26 +19,27 @@ public class Player_Shoot : MonoBehaviour {
 		if (Input.GetButtonDown("Fire1") || Input.GetKeyDown("space"))
 		{
 			
-			Player_Movement movement = new Player_Movement();
+			
 			//Player_Movement movement = GetComponent(Player_Movement); NOT WORKING.
 			
 			Rigidbody instantiatedProjectile = Instantiate(projectile,transform.position,transform.rotation *  Quaternion.Euler(270, 0, 0)) as Rigidbody;
 			
 			//create movement
-			
-			//Vector3 shootVector = new Vector3(0,0,speed);
-			//instantiatedProjectile.velocity = transform.TransformDirection(shootVector);
-			if(movement.Horiz != 0){
-				if(movement.Horiz > 0){
-					instantiatedProjectile.AddForce(transform.right * speed*2, ForceMode.Force);
-				}
-				else{
-					instantiatedProjectile.AddForce(-transform.right * speed*2, ForceMode.Force);
-				}
+			if(animation.GetState() == PlayerAnimationController.State.StandingLeft || animation.GetState() == PlayerAnimationController.State.WalkLeft){
+				//shoot left
+				instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(-speed,0,0));
+			}
+			else if(animation.GetState() == PlayerAnimationController.State.StandingRight || animation.GetState() == PlayerAnimationController.State.WalkRight){
+				instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(speed,0,0)); 
+			}
+			else if(animation.GetState() == PlayerAnimationController.State.StandingAway || animation.GetState() == PlayerAnimationController.State.WalkAway){
+				instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0,0,speed)); 
 			}
 			else{
-				instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0,instantiatedProjectile.velocity.y*2,speed)); 
+				instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0,0,-speed)); 
 			}
+			
+
 			//disable collisions between object and player
 			//Physics.IgnoreCollision(instantiatedProjectile.collider, transform.root.collider);
 			//instantiatedProjectile.collider.on
